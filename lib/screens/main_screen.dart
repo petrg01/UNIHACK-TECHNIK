49f57b772709f77def91453d8d50a8686520fa9c
Petr Gizatullin <65227475+petrg01@users.noreply.github.com>
2025-03-15 11:53:47 +1100

    Created dynamic widget template

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/dynamic_widget.dart'; // Import the updated DynamicWidget
 
 class MainScreen extends StatelessWidget {
   @override
@@ -6,9 +7,18 @@ class MainScreen extends StatelessWidget {
     return Container(
       color: Color(0xFF2c2c2e), // Dark background
       child: Center(
-        child: Text(
-          "Main Screen Content",
-          style: TextStyle(color: Colors.white, fontSize: 20), // White text
+        child: DynamicWidget(
+          width: 365, // width, default is 365
+          cornerRadius: 45, // corner radius, default is 49
+          height: 100, // height, by default as tall as the content inside
+          color: Color(0xFF50C878), // color, by default dark grey
+          child: Center(
+            child: Text(
+              "Main Screen Content",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
       ),
     );
